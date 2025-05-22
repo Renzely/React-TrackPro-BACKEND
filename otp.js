841cbaa4edc0e5073f05b52d70bc6e1d18d4b831
Renzely <ynsonharold@gmail.com>
2025-05-22 17:35:07 +0800

    v0.0.2

--- otp.js
+++ otp.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const otpSchema = new mongoose.Schema({
   email: String,
   otp: String,
+  purpose: String, // <-- Add this line
   createdAt: { type: Date, default: Date.now, index: { expires: 300 } }, // Expires in 5 minutes
 });
 
